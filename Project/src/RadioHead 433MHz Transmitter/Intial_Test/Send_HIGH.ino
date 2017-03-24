bbcf3ea027ed8f1a72388db1987fa7ce7ec477f8
Abbas Jhabuawala <abbasjhabu@gmail.com>
2017-03-24 02:45:58 +0100

    Updating Folders

---
+++ Project/src/RadioHead 433MHz Transmitter/Intial_Test/Send_HIGH.ino
@@ -0,0 +1,12 @@
+#define rfTransmitPin 12  //RF Transmitter pin = digital pin 4
+
+ void setup(){
+   pinMode(rfTransmitPin, OUTPUT);     
+ }
+
+ void loop(){
+   while(true){
+     digitalWrite(rfTransmitPin, HIGH );     //Transmit a HIGH signal
+   }
+ }
+ 
